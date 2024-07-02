c01a43d2bae8cfbaf25e765111d0de8b59092713
yoyu0625 <154570327+yoyu0625@users.noreply.github.com>
2024-07-03 02:45:58 +0800

    Update UVa10041.cpp

--- UVa10041.cpp
+++ UVa10041.cpp
@@ -5,19 +5,19 @@ using namespace std;
 int main(){
     int test;
     cin>>test;
-    int d[500]={0}; #儲存最後結果的陣列
+    int d[500]={0}; //儲存最後結果的陣列
     for(int i=0;i<test;i++){
         int r;
-        cin>>r;  #r=數據長度
-        int s[30000];   #儲存每組數據的陣列 
-        for(int j=0;j<r;j++){  #第一個數字存在s[0]直到讀取到r個數字
+        cin>>r;  //r=數據長度
+        int s[30000];   //儲存每組數據的陣列 
+        for(int j=0;j<r;j++){  //第一個數字存在s[0]直到讀取到r個數字
             cin>>s[j];  
         }
         sort(s,s+r);
         int mid;
         mid=s[(int)r/2];
         for(int j=0;j<r;j++){
-            d[i]+=abs(s[j]-mid);   #d[i]總距離和
+            d[i]+=abs(s[j]-mid);   //d[i]總距離和
         }
     }
     for(int i=0;i<test;i++){
