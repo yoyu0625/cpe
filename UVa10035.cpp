0c6dd8305fa4b76ad3894bf084a322d960f718ad
yoyu0625 <154570327+yoyu0625@users.noreply.github.com>
2024-07-03 23:10:49 +0800

    Update UVa10035.cpp

--- UVa10035.cpp
+++ UVa10035.cpp
@@ -10,14 +10,14 @@ int main(){
         ans=0;
         carry=0;
         while(a>=1||b>=1){
-            if(a%10+b%10+carry>=10){
-                ans=ans+1;
-                carry=1;
+            if(a%10+b%10+carry>=10){ //// 取 a 和 b 的末位數字加上 carry 如果結果大於等於 10
+                ans=ans+1;   //進位次數+1
+                carry=1;    //設置 carry 為 1 表示進位
             }
             else{
-                carry=0;
+                carry=0;  //沒有進位
             }
-            a=a/10;
+            a=a/10;  //去掉末位數
             b=b/10;
         }
         if(ans==0){
