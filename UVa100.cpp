9f4d0b187d504f75a7a3963d6902cef67dfe2ce4
yoyu0625 <154570327+yoyu0625@users.noreply.github.com>
2024-07-04 05:49:53 +0800

    Add files via upload

---
+++ UVa100.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int cnt;
+void f(int n){
+    cnt++;
+    if(n==1) return;
+    if(n%2!=0) f(3*n+1);
+    else f(n/2);
+}
+
+
+int main(){
+    int a,b;
+    while(cin>>a>>b){
+        int ans=0;
+        for(int i=min(a,b);i<=max(a,b);i++){
+            cnt=0;
+            f(i);
+            ans=max(ans,cnt);
+        }
+    cout<<a<<" "<<b<<" "<<ans<<endl;
+    }
+
+}
