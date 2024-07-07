655c79c77931aea68a493c4fddced0b59ee13bf5
yoyu0625 <154570327+yoyu0625@users.noreply.github.com>
2024-07-08 03:34:47 +0800

    Update UVa11321.cpp

--- UVa11321.cpp
+++ UVa11321.cpp
@@ -5,33 +5,31 @@ using namespace std;
 
 int N,M;
 bool cmp(int x,int y){
-    if((x%M)!=(y%M)){
-    	return (x%M)<(y%M);
-    	}
-    else if((x%2)*(y%2)){   //奇數
-    	return x>y;
-        }
-    else if((x%2==0)&&(y%2==0)){   //偶數
-     return x<y;}
-    else{
-        return x%2;
-    }
+	if((x%M)!=(y%M)){
+		return (x%M)<(y%M); 
+	}
+	if((x%2)*(y%2)){   //奇數
+		return x>y;
+	}
+	if((x%2==0)&&(y%2==0)){ //偶數
+		return x<y;
+	}
 }
 
 int main(){
-    while(cin>>N>>M){
-        if(N==0&&M==0){
-            cout<<"0 0"<<endl;
-            break;
-        }
-        cout<<N<<" "<<M<<endl;
-        int a[N];
-        for(int i=0;i<N;i++){
-            cin>>a[i];
-        }
-        sort(a,a+N,cmp);
-        for(int i=0;i<N;i++){
-            cout<<a[i]<<endl;
-        }
-    }
+	while(cin>>N>>M){
+		if(N==0&&M==0){
+			cout<<"0 0"<<endl;
+			break;
+		}
+		cout<<N<<" "<<M<<endl;
+		int a[N];
+		for(int i=0;i<N;i++){
+			cin>>a[i];
+		}
+		sort(a,a+N,cmp);
+		for(int i=0;i<N;i++){
+			cout<<a[i]<<endl;
+		}
+	}
 }
