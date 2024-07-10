e6610b818b4d8c2701fea6d09323065d963287b9
yoyu0625 <154570327+yoyu0625@users.noreply.github.com>
2024-07-11 00:05:27 +0800

    Update UVa11461.cpp

--- UVa11461.cpp
+++ UVa11461.cpp
@@ -11,8 +11,8 @@ int main(){
         }
         int count=0;
         for(int i=a;i<=b;i++){
-            sq=sqrt(i);
-            if(sq*sq==i){
+            sq=sqrt(i);  //計算i的平方根
+            if(sq*sq==i){  //如果平方根的平方等於i，則i是平方數
                 count++;
             }
         }
